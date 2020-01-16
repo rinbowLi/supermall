309c09a180bb1378db83813e5b2d20bbd1c8303f
lichenxin <lichenxin@insighters.cn>
2020-01-16 16:16:57 +0800

    修改接口baseurl

--- src/network/request.js
+++ src/network/request.js
@@ -3,8 +3,9 @@ import axios from 'axios'
 export function request(config) {
   //创建实例
   const server = axios.create({
-    //baseURL:"http://123.207.32.32:8000/api/v2",
-    baseURL: "http://106.54.54.237:8000/api/v1",
+  
+    baseURL : "http://123.207.32.32:8000/api/hy",
+//baseURL : "http://106.54.54.237:8000/api/hy"
     timeout:5000,
     // proxy:{    //跨域代理
     //   '/api': {
